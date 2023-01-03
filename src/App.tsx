e39d0b2ae03f023b05bf3511f58d65431f20c9f6
Almedin Hodzic <almedin.json@gmail.com>
2023-01-03 14:59:05 +0100

    showing decks on landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
+interface Deck {
+  title: string;
+  _id: string;
+}
+
 function App() {
+  const [decks, setDecks] = useState<Deck[]>([]);
   const [title, setTitle] = useState("");
 
   const handleCreateDeck = async (e: React.FormEvent) => {
@@ -16,8 +22,20 @@ function App() {
     setTitle("");
   };
 
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("http://localhost:5000/decks");
+      setDecks(await response.json());
+    })();
+  }, []);
+
   return (
     <div className="App">
+      <ul className="decks">
+        {decks.map((deck) => (
+          <li key={deck._id}>{deck.title}</li>
+        ))}
+      </ul>
       <form onSubmit={handleCreateDeck}>
         <label htmlFor="deck-title">Deck Title</label>
         <input
